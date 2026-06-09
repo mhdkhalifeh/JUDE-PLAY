2e0819dc0656c17cd018344b92c20c9289b58ae8
mhdkhalifeh <mhd.khalifeh89@gmail.com>
2026-06-09 16:34:23 +0300

    Fix ads.txt

--- app/ads.txt/route.ts
+++ app/ads.txt/route.ts
@@ -1,10 +1,13 @@
 export const dynamic = "force-static";
 
 export async function GET() {
-  return new Response("", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain; charset=utf-8",
-    },
-  });
+  return new Response(
+    "google.com, pub-1791515120755145, DIRECT, f08c47fec0942fa0",
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+      },
+    }
+  );
 }
