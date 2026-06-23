9d9b3950245d7fb50a5ce766a4e87e9aeaf524f0
mhdkhalifeh <mhd.khalifeh89@gmail.com>
2026-06-23 15:03:30 +0300

    Fix sitemap pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,15 +13,77 @@ export default async function sitemap() {
       lastModified: game.created_at,
     })) || [];
 
+  const blogUrls = [
+    "best-browser-games",
+    "why-html5-games-are-popular",
+    "top-puzzle-games",
+    "free-online-games",
+    "best-game-categories",
+    "multiplayer-browser-games",
+    "browser-games-mobile",
+    "future-of-browser-gaming",
+  ].map((slug) => ({
+    url: `${siteUrl}/blog/${slug}`,
+    lastModified: new Date(),
+  }));
+
   return [
     {
       url: siteUrl,
       lastModified: new Date(),
     },
+
     {
-      url: `${siteUrl}/favorites`,
+      url: `${siteUrl}/blog`,
       lastModified: new Date(),
     },
+
+    {
+      url: `${siteUrl}/about`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/contact`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/privacy`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/terms`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/faq`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/categories`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/top-games`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/new-games`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${siteUrl}/stats`,
+      lastModified: new Date(),
+    },
+
+    ...blogUrls,
     ...gameUrls,
   ];
 }
